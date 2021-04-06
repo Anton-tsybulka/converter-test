23a857b3f6ac133707d193cbc5d4d764f5776674
Anton Tsybulka <anton.kurto@gmail.com>
2021-04-06 11:41:35 +0300

    show nominal

--- src/components/CurrencyIHaveSelect.js
+++ src/components/CurrencyIHaveSelect.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
+import { changeCurrencyThunk1 } from '../redux/thunk/converterThunk';
 import { Select, Input } from 'antd';
 
 const { Option } = Select;
 
 const CurrencyIHaveSelect = ({ currency, value1 }) => {
     const defaultValue = currency && currency.length !== 0 && currency[10].CharCode;
-
-    const handleChange = (value) => {
-        /* setNominal1(nominalShow(value, currency)); */
-    };
+    const dispatch = useDispatch();
 
     return (
         <div>
@@ -18,13 +17,14 @@ const CurrencyIHaveSelect = ({ currency, value1 }) => {
                 <Select
                     defaultValue={defaultValue}
                     style={{ width: 120 }}
-                    onChange={handleChange}>
+                    onChange={(value) => dispatch(changeCurrencyThunk1(value))}>
                     {currency && currency.map(({ ID, CharCode }) => {
                         return <Option key={ID} value={CharCode}>{CharCode}</Option>
                     })}
                 </Select>}
             <Input
                 placeholder={value1}
+                onChange={(value) => console.log(value.target.value)}
                 style={{ width: 120, borderStyle: 'none', borderBottom: '1px solid rgb(217, 217, 217)' }} />
         </div>
     )
